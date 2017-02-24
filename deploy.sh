66c623adf13b3434fdb19bdb58f96c2387c4bc3e
Karl Gustav Roksund <KarlGustav@unimicro.no>
2017-02-24 14:52:21 +0100

    Add "set -e" to the build script so they fail fast

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 
 scp -r dist/* unidiv:/tmp/unibot-tmp/ \
     && ssh unidiv 'sudo mv /tmp/unibot-tmp/* /srv/unibot/ 2>/dev/null; sudo mv /srv/unibot/unibot{,.old} ; sudo mv /srv/unibot/unibot{.new,} && sudo service unibot restart'
