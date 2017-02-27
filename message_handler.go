9f3b527fd39bfc6425e59cae84cab61340a889b5
Karl Gustav Roksund <KarlGustav@unimicro.no>
2017-02-27 13:40:06 +0100

    Add answer to traveltext inquiries that is made in pubic channels
    Because we want to help users that haven't understood that the
    traveltext communication needs to be private and is therefore happening
    in the @unibot private channel.
    
    Also changed the DIRECT_CHANNEL_PREFIX to directChannelPrefix because
    that is how constants is written in go.

--- message_handler.go
+++ message_handler.go
@@ -8,11 +8,15 @@ import (
 )
 
 const (
-	DIRECT_CHANNEL_PREFIX = "D"
+	directChannelPrefix = "D"
+	publicChannelPrefix = "C"
 )
 
 func handleMessage(ev *slack.MessageEvent, rtm *slack.RTM) {
-	if strings.HasPrefix(strings.ToLower(ev.Text), traveltext.TravelTextCommandPrefix) && strings.HasPrefix(ev.Channel, DIRECT_CHANNEL_PREFIX) {
+	if strings.HasPrefix(strings.ToLower(ev.Text), traveltext.TravelTextCommandPrefix) && strings.HasPrefix(ev.Channel, directChannelPrefix) {
 		traveltext.HandleTravelTextCommand(ev, rtm)
+	}
+	if strings.HasPrefix(ev.Text, "<@U3C7XCU5S> tt ") && strings.HasPrefix(ev.Channel, publicChannelPrefix) {
+		rtm.SendMessage(rtm.NewOutgoingMessage("You're trying to send a TravelText command in a public channel, please talk to me privatly for that: https://unimicro.slack.com/messages/@unibot", ev.Channel))
 	}
 }
