bcb115b9acea9afecca8599fd07d435e1583431a
Karl Gustav Roksund <KarlGustav@unimicro.no>
2017-09-08 12:06:44 +0200

    Look for standalone Jira keys and reply with full Jira URLs
    Because searching for them is a pain...

--- message_handler.go
+++ message_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/nlopes/slack"
@@ -18,5 +19,16 @@ func handleMessage(ev *slack.MessageEvent, rtm *slack.RTM) {
 	}
 	if strings.HasPrefix(ev.Text, "<@U3C7XCU5S> tt ") && strings.HasPrefix(ev.Channel, publicChannelPrefix) {
 		rtm.SendMessage(rtm.NewOutgoingMessage("You're trying to send a TravelText command in a public channel, please talk to me privatly for that: https://unimicro.slack.com/messages/@unibot", ev.Channel))
+	}
+
+	r, _ := regexp.Compile("^[[:alpha:]]{2,3}-\\d+|\\s[[:alpha:]]{1,3}-\\d+")
+	matches := r.FindAllString(ev.Text, -1)
+	if len(matches) > 0 && strings.HasPrefix(ev.Channel, publicChannelPrefix) {
+		for _, m := range matches {
+			rtm.SendMessage(rtm.NewOutgoingMessage(
+				"https://unimicro.atlassian.net/browse/"+strings.ToUpper(strings.TrimSpace(m)),
+				ev.Channel,
+			))
+		}
 	}
 }
