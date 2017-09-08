33c43e31a340eee0a0beffaa09f40156f98b0455
Karl Gustav Roksund <KarlGustav@unimicro.no>
2017-09-08 13:05:53 +0200

    Cleanup of message_handler.go after code review

--- message_handler.go
+++ message_handler.go
@@ -11,24 +11,33 @@ import (
 const (
 	directChannelPrefix = "D"
 	publicChannelPrefix = "C"
+	uniBotUserId        = "<@U3C7XCU5S>"
 )
 
-func handleMessage(ev *slack.MessageEvent, rtm *slack.RTM) {
-	if strings.HasPrefix(strings.ToLower(ev.Text), traveltext.TravelTextCommandPrefix) && strings.HasPrefix(ev.Channel, directChannelPrefix) {
-		traveltext.HandleTravelTextCommand(ev, rtm)
-	}
-	if strings.HasPrefix(ev.Text, "<@U3C7XCU5S> tt ") && strings.HasPrefix(ev.Channel, publicChannelPrefix) {
-		rtm.SendMessage(rtm.NewOutgoingMessage("You're trying to send a TravelText command in a public channel, please talk to me privatly for that: https://unimicro.slack.com/messages/@unibot", ev.Channel))
-	}
+var jiraKeyRegex = regexp.MustCompile("^[[:alpha:]]{2,3}-\\d+|\\s[[:alpha:]]{1,3}-\\d+")
 
-	r, _ := regexp.Compile("^[[:alpha:]]{2,3}-\\d+|\\s[[:alpha:]]{1,3}-\\d+")
-	matches := r.FindAllString(ev.Text, -1)
-	if len(matches) > 0 && strings.HasPrefix(ev.Channel, publicChannelPrefix) {
-		for _, m := range matches {
-			rtm.SendMessage(rtm.NewOutgoingMessage(
-				"https://unimicro.atlassian.net/browse/"+strings.ToUpper(strings.TrimSpace(m)),
-				ev.Channel,
-			))
-		}
+func handleMessage(ev *slack.MessageEvent, rtm *slack.RTM) {
+
+	switch {
+	case strings.HasPrefix(strings.ToLower(ev.Text), traveltext.TravelTextCommandPrefix) && strings.HasPrefix(ev.Channel, directChannelPrefix):
+		traveltext.HandleTravelTextCommand(ev, rtm)
+	case strings.HasPrefix(ev.Text, uniBotUserId+" tt ") && strings.HasPrefix(ev.Channel, publicChannelPrefix):
+		handleTravelTextInPulicChannel(ev, rtm)
+	case strings.HasPrefix(ev.Channel, publicChannelPrefix):
+		handleJiraKeyInMessage(ev, rtm)
+	}
+}
+
+func handleTravelTextInPulicChannel(ev *slack.MessageEvent, rtm *slack.RTM) {
+	rtm.SendMessage(rtm.NewOutgoingMessage("You're trying to send a TravelText command in a public channel, please talk to me privatly for that: https://unimicro.slack.com/messages/@unibot", ev.Channel))
+}
+
+func handleJiraKeyInMessage(ev *slack.MessageEvent, rtm *slack.RTM) {
+	matches := jiraKeyRegex.FindAllString(ev.Text, -1)
+	for _, m := range matches {
+		rtm.SendMessage(rtm.NewOutgoingMessage(
+			"https://unimicro.atlassian.net/browse/"+strings.ToUpper(strings.TrimSpace(m)),
+			ev.Channel,
+		))
 	}
 }
